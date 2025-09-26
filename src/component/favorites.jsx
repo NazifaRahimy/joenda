7637a9f0f469af90b677752a33f483a5306507c8
nazifaRahimi <nazifarahimy6@gmail.com>
2025-09-26 22:44:08 +0430

    Add md:rounded-md for responsive border radius

--- src/component/favorites.jsx
+++ src/component/favorites.jsx
@@ -141,7 +141,7 @@ function FavoritePage({cart}) {
                       src={product.images[0].url}
                       alt={product.product}
                       // src={`http://localhost:3001${product.images[0].url}`}
-                      className={`w-[200px] md:w-full h-[150px] object-cover md:mt-5 rounded-md  ${
+                      className={`w-[200px] md:w-full h-[150px] object-cover md:mt-5 md:rounded-md  ${
                         document.documentElement.dir === "rtl"
                           ? "rounded-tl-md rounded-bl-md"
                           : "rounded-tr-md rounded-br-md"
